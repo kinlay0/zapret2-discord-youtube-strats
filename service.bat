735e4edd6637ce44a8db2db920c3d051c7b033b4
kinlay0 <92862763+kinlay0@users.noreply.github.com>
2026-08-11 01:47:03 +0300

    clarify status

--- service.bat
+++ service.bat
@@ -79,7 +79,7 @@ echo   :: SETTINGS
 echo      4. Game Filter         [!GameFilterStatus!]
 echo      5. IPSet Filter        [!IPsetStatus!]
 echo      6. Auto-Update Check   [!CheckUpdatesStatus!]
-echo      7. Replace active fakes
+echo      7. Replace active fakes (not working)
 echo.
 echo   :: UPDATES
 echo      8. Update IPSet List
@@ -87,8 +87,8 @@ echo      9. Update Hosts File
 echo      10. Check for Updates
 echo.
 echo   :: TOOLS
-echo      11. Run Diagnostics
-echo      12. Run Tests
+echo      11. Run Diagnostics (may not work properly)
+echo      12. Run Tests (not working)
 echo.
 echo   ----------------------------------------
 echo      0. Exit
