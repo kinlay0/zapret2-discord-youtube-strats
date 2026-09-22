21b31fc7cd2f843b02ebe54d0a165708cc9a51be
kinlay0 <92862763+kinlay0@users.noreply.github.com>
2026-09-22 18:34:41 +0300

    add new fake in blobs.lua

--- lua/blobs.lua
+++ lua/blobs.lua
@@ -24,6 +24,7 @@ for name, file in pairs{
 	tls_5ka         = "tls_clienthello_5ka_ru.bin",
 	tls_mail        = "tls_clienthello_sochi_park.bin",
 	tls_sferum      = "tls_clienthello_www_sferum_ru.bin",
+	tls_retext      = "tls_clienthello_retext_ai.bin",
 	quic_5ka        = "quic_initial_5ka_ru.bin",
 	quic_google     = "quic_initial_www_google_com.bin",
 	quic_4pda       = "quic_initial_4pda_to.bin",
